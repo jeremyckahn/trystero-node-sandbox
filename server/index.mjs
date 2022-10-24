2294f773a665ca21d4230d3e619fe878dac95286
Jeremy Kahn <jeremyckahn@gmail.com>
2022-10-23 21:33:57 -0500

    chore: use Node 19

--- server/index.mjs
+++ server/index.mjs
@@ -1,4 +1,12 @@
 import Koa from 'koa'
+import { joinRoom } from 'trystero'
 const app = new Koa()
+
+joinRoom(
+  {
+    appId: 'trystero-node-support-testing',
+  },
+  'global'
+)
 
 app.listen(3500)
